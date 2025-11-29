8580e531fdd24e0491ffcf62b75525c4c92997ff
officialayush23 <officialayushp123@gmail.com>
2025-11-29 21:10:07 +0530

    yo yo

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -5,12 +5,13 @@ import './index.css';
 import App from './App.jsx';
 import { ThemeProvider } from './components/use_ui/ThemeProvider.jsx';
 import { Toaster } from 'sonner';
-
+import { Analytics } from "@vercel/analytics/next"
 
 
 createRoot(document.getElementById('root')).render(
 
   <BrowserRouter>
+  <Analytics/>
     <ThemeProvider>
       {/* <StrictMode> */}
       <App />
