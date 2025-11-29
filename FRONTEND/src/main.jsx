6f4916785b81e948855390e4526752b06061b3a3
officialayush23 <officialayushp123@gmail.com>
2025-11-29 21:38:51 +0530

    yo yo

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 import { ThemeProvider } from './components/use_ui/ThemeProvider.jsx';
 import { Toaster } from 'sonner';
 
-
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
 
@@ -14,6 +14,7 @@ createRoot(document.getElementById('root')).render(
 
     <ThemeProvider>
       {/* <StrictMode> */}
+      <Analytics />
       <App />
       {/* </StrictMode> */}
       <Toaster/>
